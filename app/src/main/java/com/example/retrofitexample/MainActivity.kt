ab4e60c03b200f2f935f6d7fa36a40af614b92bb
ebruaeo <ebruaeo@hotmail.com>
2024-08-28 14:47:47 +0300

    updateUserDetails/createUser/fetchUser functions

--- app/src/main/java/com/example/retrofitexample/MainActivity.kt
+++ app/src/main/java/com/example/retrofitexample/MainActivity.kt
@@ -23,6 +23,9 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         fetchUserDetails(1)
+        updateUserDetails(1, User(1, "New name", "newname", "newname@example.com"))
+        createUser(User(3, "sdf name", "asfhu", "newname@example.com"))
+        fetchUsers(5)
     }
 
     private fun fetchUserDetails(userId: Int) {
@@ -51,5 +54,71 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun updateUserDetails(userId: Int, user: User) {
+        val call = RetrofitClient.instance.updateUserDetails(userId, user)
+
+        call.enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    Log.d("MainActivity", "User updated: ${response.body()?.name}")
+                } else {
+                    Toast.makeText(this@MainActivity, "Failed to update user", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    private fun createUser(user: User) {
+        val call = RetrofitClient.instance.createUser(user)
+
+        call.enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    Log.d("MainActivity", "User created: ${response.body()?.name}")
+                } else {
+                    Toast.makeText(this@MainActivity, "Failed to create user", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    private fun fetchUsers(page: Int) {
+        val call = RetrofitClient.instance.getUsers(page)
+
+        call.enqueue(object : Callback<List<User>> {
+            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        for (user in it) {
+                            Log.d("MainActivity", "User: ${user.name}")
+                        }
+                    }
+                } else {
+                    Toast.makeText(this@MainActivity, "Failed to fetch users", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+
     }
 }
